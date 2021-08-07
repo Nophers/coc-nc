2381cd16188f688f30598c499f3296af26708eeb
Konrad570 <Konradtoesch@icloud.com>
2021-08-07 14:35:48 +0200

    update

--- cogs/CoC.py
+++ cogs/CoC.py
@@ -5,25 +5,26 @@ import mariadb
 from time import time
 import asyncio
 import aiohttp
+from main import config
 
 
 class CocDB:
 
     def __init__(self):
-        self.COCDB = 'suse'
-        self.API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2YmRjYjMxLWRkYTQtNDRjZi1hNDM4LTc2ZjVjZDhlNjA0OCIsImlhdCI6MTYyNzY4OTU2OCwic3ViIjoiZGV2ZWxvcGVyLzgwNDFiZTZhLWJiMDktMzkyYS03ZmYyLTVlZGIwMDU0Njg4YiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjUuODMuMTkxLjEwMSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.oUm4Y4sHamlcAkOHu9-TMxOY0yGQMkSwgYv_v-vEREfOevrKJP7KsOBCN2Od7E4RNC9S93SdyQcjEdE-7YHzTw'
+        self.COCDB = config.config['db']
+        self.API_KEY = config.config['key']
         self.FETCHPLAYER_API_URL = f'https://api.clashofclans.com/v1/players/'
         self.db = mariadb.connect(
-            user='test', host='localhost', password="newpassword", port=3306)
+            user=config.config['user'], 
+            host=config.config['host'], 
+            password=config.config['password'],
+            port=30449)
         self.c = self.db.cursor(buffered=True)
-        self.c.execute(f'CREATE DATABASE IF NOT EXISTS `{self.COCDB}`')
         self.db.database = self.COCDB
         self.c.execute('SET AUTOCOMMIT=1')
         self.c.execute(
             'ALTER DATABASE {} CHARACTER SET utf8 COLLATE utf8_unicode_ci;'.format(self.COCDB))
-        self.c.execute('''CREATE TABLE IF NOT EXISTS test(
-            teststr TEXT,
-            testint TEXT)''')
+        self.c.execute('SET SESSION wait_timeout = 999999999999')
 
 
 cocdb = CocDB().c
@@ -43,18 +44,70 @@ class CoC(commands.Cog):
             '2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12',
             '2021-01', '2021-02', '2021-03', '2021-04', '2021-05', '2021-06', '2021-07']  # ,'2021-08','2021-09','2021-10','2021-11','2021-12']
 
+    async def searchtagbyseason(self, season, tag):
+        cocdb.execute(f'SELECT * FROM `{season}` WHERE tag = "{tag}"')
+        result = cocdb.fetchone()
+        if result:
+            return {'season': season, 'data': result}
+
     @commands.command()
     @commands.cooldown(2, 15, commands.BucketType.guild)
     @commands.guild_only()
     async def tag(self, ctx, tag):
-        ...
-        
-    @commands.command()
-    async def testcall(self, ctx, a,b):
-        cocdb.execute('INSERT INTO test (teststr,testint) VALUES (?,?)',(a,b))
-        cocdb.execute('SELECT * FROM test')
-        result = cocdb.fetchall()
-        await ctx.send(result)
+        try:
+            tag = '#' + tag.replace('#', '')
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'{rawcocdb.FETCHPLAYER_API_URL}{tag.replace("#","%23")}',headers={"Accept": "application/json", "authorization": f"Bearer {rawcocdb.API_KEY}"}) as request:
+                    if not (request.status == 200):
+                        print(request)
+                        return await ctx.send(embed=embeds.Error._text_to_embed(self.bot, ctx, 'This player doesn\'t exist.'))
+                    r = await request.json()
+                    playerName = r['name']
+            s = time()
+            msg = await ctx.send(embed=embeds.Loading._text_to_embed(self.bot, ctx, f'Searching through 20m+ user records for the player **{playerName}**, this could take up to 30s...'))
+            async with ctx.typing():
+                results =  [await self.searchtagbyseason(season, tag) for season in self.LEGEND_SEASONS]
+                e = time()
+                results = [result for result in results if result != None]
+                if not results:
+                    return await ctx.send(embed=embeds.Error._text_to_embed(self.bot, ctx, f'User **{playerName}** not found in our database. Note: User must\'ve been in the legend league once.'))
+                embed = discord.Embed(description=', '.join(
+                    '`' + x['season'] + '`' for x in results) + '\n\n✅ **Show all results**\n❌ **Delete this embed**', color=DEFAULT_COLOR, timestamp=ctx.message.created_at)
+                embed.set_footer(text=f'Took {round(e-s,3)}s | Requested by {ctx.author.name}')
+                embed.set_author(name=f'Seasons of the player {playerName} [{tag}]',icon_url=ctx.author.avatar_url)
+                await msg.edit(embed=embed)
+                await msg.add_reaction('✅')
+                await msg.add_reaction('❌')
+                await asyncio.sleep(0.5)
+                try:
+                    while 1:
+                        reaction = await self.bot.wait_for('reaction_add', timeout=60)
+                        if reaction[0].message.id == msg.id:
+                            if str(reaction[0].emoji) == '❌':
+                                await msg.delete()
+                            if str(reaction[0].emoji) == '✅':
+                                for result in results:
+                                    embed = discord.Embed(title=f'Information about the player {playerName} [{tag}]',
+                                                          description=f'This dataset is from **{result["season"]}**',
+                                                          timestamp=ctx.message.created_at,
+                                                          color=DEFAULT_COLOR)
+                                    embed.add_field(name='Tag',value=tag)
+                                    embed.add_field(name='Name',value=result["data"][1])
+                                    embed.add_field(name='XP',value=result["data"][2])
+                                    # embed.add_field(name='Trophies', value=result["data"][3])
+                                    # embed.add_field(name='Attack wins',value=result["data"][4])
+                                    # embed.add_field(name='Defense wins',value=result["data"][5])
+                                    # embed.add_field(name='Global rank',value=result["data"][6])
+                                    embed.add_field(name='Clantag',value=result["data"][7])
+                                    embed.add_field(name='Clanname',value=result["data"][8])
+                                    #embed.add_field(name='Clan badge', value=f'[Link]({result["data"][9]})')
+                                    embed.set_footer(text=f'Requested by {ctx.author.name}')
+                                    await ctx.send(embed=embed)
+                                return
+                except asyncio.TimeoutError:
+                    ___ = [await msg.remove_reaction(reaction) for reaction in ['✅', '❌'] if reaction in msg.reactions]
+        except Exception as e:
+            print('ERROR ' + str(e))
 
 
 def setup(bot):
